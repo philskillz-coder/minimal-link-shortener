b0f73754c82d3ca062efe5f5fb4088cc55cf8b2a
Philskillz <76884393+philskillz-coder@users.noreply.github.com>
2022-12-20 19:22:06 +0100

    added custom url names

--- drivers/base.py
+++ drivers/base.py
@@ -8,7 +8,7 @@ class BaseDriver:
     async def setup(self):
         pass
 
-    async def create_url(self, url: str) -> str:
+    async def create_url(self, url: str, name: Optional[str] = None) -> Optional[str]:
         raise NotImplementedError("Not Implemented")
 
     async def get_url(self, name: str) -> Optional[str]:
