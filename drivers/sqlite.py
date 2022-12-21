a6b7f32498c4c67d5296213cb644120a9b26e68e
Philskillz <76884393+philskillz-coder@users.noreply.github.com>
2022-12-21 10:24:34 +0100

    new sqlite driver working

--- drivers/sqlite.py
+++ drivers/sqlite.py
@@ -41,7 +41,7 @@ class Driver(BaseDriver):
     ]
     NAME = "sqlite"
 
-    __table__ = "CREATE TABLE IF NOT EXISTS \"urls\" (\"id\" INTEGER NOT NULL UNIQUE, \"url\" TEXT NOT NULL, " \
+    __table__ = "CREATE TABLE IF NOT EXISTS \"urls\" (\"id\" INTEGER NOT NULL UNIQUE, name TEXT UNIQUE, \"url\" TEXT NOT NULL, " \
                 "PRIMARY KEY(\"id\" AUTOINCREMENT))"
 
     def __init__(self, file: str, length: int, secret: str, alphabet: str):
@@ -54,21 +54,33 @@ class Driver(BaseDriver):
         self.connection: aiosqlite.Connection = await aiosqlite.connect(self.file)
         await self.connection.execute(self.__table__)
 
-    async def create_url(self, url: str) -> str:
-        async with self.connection.execute("INSERT INTO urls(url) VALUES(?) RETURNING id;", (url, )) as cursor:
+    async def create_url(self, url: str, name: Optional[str] = None) -> Optional[str]:
+        if name is not None:
+            async with self.connection.execute("SELECT EXISTS(SELECT 1 FROM urls WHERE name = ?)", (name, )) as cursor:
+                exists, = await cursor.fetchone()
+                if not exists:
+                    await cursor.execute("INSERT INTO urls(name, url) VALUES(?, ?)", (name, url))
+                    return name
+                return None
+        async with self.connection.execute("INSERT INTO urls(url) VALUES(?)", (url, )) as cursor:
             _id = cursor.lastrowid
 
         await self.connection.commit()
         return self.hashids.encode(_id)
 
     async def get_url(self, name: str) -> Optional[str]:
-        decoded = self.hashids.decode(name)
-        if not decoded:
-            return None
+        async with self.connection.execute("SELECT url FROM urls WHERE name = ?", (name, )) as cursor:
+            url = await cursor.fetchone()
+            if url is not None:
+                return url[0]
 
-        async with self.connection.execute("SELECT url FROM urls WHERE id = ?", (decoded[0],)) as cursor:
+            decoded = self.hashids.decode(name)
+            if not decoded:
+                return None
+
+            await cursor.execute("SELECT url FROM urls WHERE id = ?", (decoded[0],))
+
             url = await cursor.fetchone()
             if url is None:
                 return None
-
             return url[0]
