ee2fc877b5dd430b530ac6bf22438b16578d4873
Philskillz <76884393+philskillz-coder@users.noreply.github.com>
2022-12-21 10:29:40 +0100

    added support for custom url names

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from quart import Quart, request, jsonify, redirect, abort
 import json
 import os
@@ -14,9 +16,9 @@ with open("config.json", "r") as f:
     config = json.load(f)
 
 driver: drivers.base.BaseDriver = importlib.import_module(
-    config["driver_config"]["driver"]
+    config["driver"]["using"]
 ).Driver(
-    **config["driver_config"]["args"]
+    **config["driver"][config["driver"]["using"]]
 )
 app = Quart(__name__)
 
@@ -33,11 +35,13 @@ async def create():
         return jsonify({"message": "Unauthorized"}), 401
 
     body = await request.form
-    url = body.get("url")
+    url: Optional[str] = body.get("url")
     if url is None:
         return jsonify({"message": "Missing Data"}), 405
 
-    shortened_url = await driver.create_url(url)
+    name: Optional[str] = body.get("name") or None
+
+    shortened_url = await driver.create_url(url, name)
 
     return jsonify({"message": "Success", "code": shortened_url}), 200
 
